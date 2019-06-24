68d0e3583464a7e40969c8a5074ee4a057e1065d
JaeHoon <xpiona@naver.com>
2019-06-24 12:15:42 +0900

    practice

---
+++ test_module.py
@@ -0,0 +1,15 @@
+﻿"""
+모듈: module_test
+circle_area(반지름) : 원의 넓이
+circumference(반지름) : 원의 둘레
+"""
+
+pi =  3.131592653589793238462643383279
+
+def circle_area(r):
+	a_result = pi * r ** 2
+	return a_result
+
+def circumference(r):
+	c_result = pi * r * 2
+	return c_result
