8b4500db3dbc3f5cf953b668b85ec404d4c4be49
JaeHoon <45818709+xpiona@users.noreply.github.com>
2019-07-26 11:24:20 +0900

    Create 07_25_3rd.py

---
+++ 07_25_3rd.py
@@ -0,0 +1,8 @@
+import os
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Embedding, SimpleRNN, LSTM
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+import numpy as np
+import matplotlib.pyplot as plt
